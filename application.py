9d3def4ec9a3fb295078b66060bb5fdf14779b78
unknown <mitch.traylor@utexas.edu>
2018-02-12 19:38:17 -0600

    Updated application.py to include new instances.

--- application.py
+++ application.py
@@ -30,6 +30,51 @@ def majors():
     return render_template('majors.html')
 
 
+@application.route('/city-austin')
+def city_austin():
+    return render_template('city-Austin.html')
+
+
+@application.route('/city-houston')
+def city_houston():
+    return render_template('city-Houston.html')
+
+
+@application.route('/city-colstat')
+def city_colstat():
+    return render_template('city-CollegeStation.html')
+
+
+@application.route('/college-utexas')
+def college_ut():
+    return render_template('college-utexas.html')
+
+
+@application.route('/college-texasam')
+def college_tam():
+    return render_template('college-texasAM.html')
+
+
+@application.route('/college-uhouston')
+def college_uh():
+    return render_template('college-uhouston.html')
+
+
+@application.route('/major-compsci')
+def major_computer_science():
+    return render_template('major-compsci.html')
+
+
+@application.route('/major-nursing')
+def major_nursing():
+    return render_template('major-nursing.html')
+
+
+@application.route('/major-petroleumengineering')
+def major_petroleum_engineering():
+    return render_template('major-petrolengineering.html')
+
+
 @application.route('/api/about')
 def api_about():
     return git.get_commit_stat()
