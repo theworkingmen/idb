8f2c089acedae7aa170da8e5bf9727f120b4bdf8
Sungsup <smcw66@gmail.com>
2018-03-05 17:29:36 -0600

    added default pie chart to college instance

--- src/js/CollegeInstance.js
+++ src/js/CollegeInstance.js
@@ -1,28 +1,74 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import InstanceTitle from './InstanceTitle.js';
+import Chart from './Chart.js';
+import {Jumbotron, Row, Col } from 'react-bootstrap';
 import '../css/App.css';
 
 class CollegeInstance extends Component {
 
-	
+	constructor(){
+    super();
+    this.state = {
+      chartData:{}
+    }
+  }
+
+  componentWillMount(){
+    this.getChartData();
+  }
+
+  getChartData(){
+    // Ajax calls here
+    this.setState({
+      chartData:{
+        labels: ['White', 'Asian', 'Hispanic', 'African American', 'Other'],
+        datasets:[
+          {
+            label:'Population',
+            data:[
+              617594,
+              181045,
+              153060,
+              106519,
+              105162,
+              95072
+            ],
+            backgroundColor:[
+              'rgba(255, 99, 132, 0.6)',
+              'rgba(54, 162, 235, 0.6)',
+              'rgba(255, 206, 86, 0.6)',
+              'rgba(75, 192, 192, 0.6)',
+              'rgba(153, 102, 255, 0.6)',
+              'rgba(255, 159, 64, 0.6)'
+            ]
+          }
+        ]
+      }
+    });
+  }
 	
 	render() {
 		return (
-		    <div className="container">
-		       <InstanceTitle name={this.props.match.params.name}/>
-		       
-		       <p id="summary"> Summary: insert api call here </p>
-		       <p id="city"> City: insert api call here  </p>
-		       <p id="state"> State: insert api call here  </p>
-		       <p id="social_media"> Social Media: insert api call here  </p>
-		       <p id="resturants"> Number of Resturants: insert api call here  </p>
-		       <p id="ranking"> National Ranking: insert api call here  </p>
-		       <p id="acceptance"> Acceptance Rate: insert api call here  </p>
-		       <p id="tuition_in"> Tuition(in-state): insert api call here  </p>
-		       <p id="tuition_out"> Tuition(out-of-state): insert api call here  </p>
-		       
-		    </div>
+	    <div className={[container, info].join(' ')}>
+		    	
+				<center><InstanceTitle name={this.props.match.params.name}/></center>
+
+				<Col md={6}>
+					<p id="summary"> Summary: insert api call here </p>
+					<p id="city"> City: insert api call here  </p>
+					<p id="state"> State: insert api call here  </p>
+					<p id="social_media"> Social Media: insert api call here  </p>
+					<p id="resturants"> Number of Resturants: insert api call here  </p>
+					<p id="ranking"> National Ranking: insert api call here  </p>
+					<p id="acceptance"> Acceptance Rate: insert api call here  </p>
+					<p id="tuition_in"> Tuition(in-state): insert api call here  </p>
+					<p id="tuition_out"> Tuition(out-of-state): insert api call here  </p>
+				</Col>
+				<Col md={6}>
+		    	<Chart chartData={this.state.chartData} titleText="University Demographic" legendPosition="right"/>
+		    </Col>
+	    </div>
 		);
 	}
 }
--- src/js/MajorInstance.js
+++ src/js/MajorInstance.js
@@ -1,24 +1,34 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
+import {Jumbotron, Row, Col} from 'react-bootstrap';
 import InstanceTitle from './InstanceTitle.js';
 import '../css/App.css';
+import Chart from './Chart.js';
 
 class MajorInstance extends Component {
+
 
 	
 	
 	render() {
+
 		return (
-		    <div className="container">
-		       <InstanceTitle name={this.props.match.params.name}/>
-		       
-		       <p id="summary"> Summary: insert api call here </p>
-		       <p id="employ_rate"> Employment Rate: insert api call here </p>
-		       <p id="relevance"> Relevance in Industry: insert api call here </p>
-		       <p id="salaries"> Salaries: insert api call here </p>
-		       
-		       
-		    </div>
+			<div className="container">
+		    
+		    <Jumbotron>
+		    	<center> <InstanceTitle name={this.props.match.params.name}/> </center>
+		      
+		      <p id="summary"> Summary: insert api call here </p>
+		      <p id="employ_rate"> Employment Rate: insert api call here </p>
+		      <p id="relevance"> Relevance in Industry: insert api call here </p>
+		      <p id="salaries"> Salaries: insert api call here </p>
+	      	
+		    </Jumbotron>
+		    
+		  </div>
+		  
+		  
+		  
 		);
 	}
 }
