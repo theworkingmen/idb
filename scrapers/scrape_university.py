8a9e56a109f21cbba064dfde4f61179311155b98
abelhtt <abelhaddistt@gmail.com>
2018-03-06 20:39:15 -0600

    Add race demographics to university data

---
+++ scrapers/scrape_university.py
@@ -0,0 +1,201 @@
+import requests
+import json
+import math
+import keys
+
+
+universities_list = []
+
+def get_university_stat():
+    url_basic_uni_info = 'http://api.datausa.io/attrs/university'
+
+    response = requests.get(url_basic_uni_info)
+
+    basic_data = json.loads(response.text)
+
+    uni_list = basic_data["data"]
+
+    for uni in uni_list:
+        sector = int(uni[0])
+        has_url = uni[2] is not None
+        university_dict = {}
+        if ((sector == 1) or (sector == 2) or (sector == 3)) and has_url:
+            university_dict["name"] = uni[1]
+            university_dict["website"] = uni[2]
+            university_dict["county_id"] = uni[4]
+            university_dict["state_id"] = uni[5]
+            university_dict["latitude"] = uni[6]
+            university_dict["longitude"] = uni[7]
+            university_dict["university_id"] = uni[8]
+            university_dict["city_id"] = uni[9]
+            universities_list.append(university_dict)
+
+    url_get_state = 'http://api.datausa.io/attrs/geo/'
+    for i in range(0, len(universities_list)):
+        try:
+            uni = universities_list[i]
+            id = uni["state_id"]
+            temp = url_get_state + str(id)
+
+            response = requests.get(temp)
+            state_data = json.loads(response.text)
+            print(state_data["data"][0][1])
+            uni["state"] = state_data["data"][0][1]
+            del uni["state_id"]
+        except Exception as e:
+            pass
+
+    with open('university.json', 'w') as f:
+        json.dump(universities_list, f)
+
+def add_tuition_stat():
+
+    temp_dict = {}
+
+    url_get_tuition = 'http://api.datausa.io/api/?show=university&sumlevel=all&year=latest&required=oos_tuition,state_tuition'
+
+    response = requests.get(url_get_tuition)
+    tuition_data = json.loads(response.text)
+
+    uni_data = tuition_data['data']
+
+    for data in uni_data:
+        if data is not None:
+            tuition_list = [0, 0, 0]
+            tuition_list[0] = data[3] # oos
+            tuition_list[1] = data[4] # in state
+            tuition_list[2] = data[0] # survey_year
+            temp_dict[data[2]] = tuition_list
+        else:
+            print("NULL")
+
+    with open('university.json', 'r') as f:
+         uni_total_data = json.load(f)
+
+    for data_curr in uni_total_data:
+        try:
+            tuition_list = temp_dict[data_curr["university_id"]]
+            data_curr["oos_tuition"] = tuition_list[0]
+            data_curr["state_tuition"] = tuition_list[1]
+            data_curr["survey_year"] = tuition_list[2]
+
+        except Exception as e:
+            data_curr["oos_tuition"] = None
+            data_curr["state_tuition"] = None
+            data_curr["survey_year"] = None
+            pass
+
+
+    with open('university.json', 'w') as fi:
+         json.dump(uni_total_data, fi)
+
+def scrape_race_stat():
+
+    url_get_race = ('https://api.data.gov/ed/collegescorecard/v1/schools?fields=school.name'+
+    ',id,2015.student.demographics.race_ethnicity.white,2015.student.demographics.race_ethnicity.'+
+    'black,2015.student.demographics.race_ethnicity.hispanic,2015.student.demographics.race_ethnicity.'+
+    'asian,2015.student.demographics.race_ethnicity.aian,2015.student.demographics.race_ethnicity.nhpi,'+
+    '2015.student.demographics.race_ethnicity.two_or_more,2015.student.demographics.race_ethnicity.'+
+    'non_resident_alien,2015.student.demographics.race_ethnicity.unknown,2015.student.demographics.'+
+    'race_ethnicity.white_non_hispanic,2015.student.demographics.race_ethnicity.black_non_hispanic,2015'+
+    '.student.demographics.race_ethnicity.asian_pacific_islander&sort=2015.completion.rate_suppressed.'+
+    'overall:desc&page=')
+
+    key = keys.data_gov_key
+    response = requests.get(url_get_race+str(0)+key)
+    race_data = json.loads(response.text)
+
+    total_items = race_data["metadata"]["total"]
+    per_page = race_data["metadata"]["per_page"]
+    pages = math.ceil(int(total_items) / int(per_page))
+
+    results = []
+
+    for i in range (0, pages):
+        print("page " + str(i))
+        page_url = url_get_race + str(i) + key
+        response = requests.get(page_url)
+        race_data = json.loads(response.text)
+        results += race_data["results"]
+
+
+    with open('temp_race_data.json', 'w') as f:
+        json.dump(results, f)
+
+def add_race_stat():
+    temp_dict = {}
+    with open('university.json', 'r') as f:
+         uni_total_data = json.load(f)
+
+    with open('temp_race_data.json', 'r') as f:
+         race_data = json.load(f)
+
+    for uni in race_data:
+        dic = {}
+        id = uni['id']
+        dic['white'] = uni["2015.student.demographics.race_ethnicity.white"]
+        dic['black'] = uni["2015.student.demographics.race_ethnicity.black"]
+        dic['asian'] = uni["2015.student.demographics.race_ethnicity.asian"]
+        dic['hispanic'] = uni["2015.student.demographics.race_ethnicity.hispanic"]
+
+        summ = 0
+        if uni["2015.student.demographics.race_ethnicity.aian"] is not None:
+            summ += uni["2015.student.demographics.race_ethnicity.aian"]
+
+        if uni["2015.student.demographics.race_ethnicity.nhpi"] is not None:
+            summ += uni["2015.student.demographics.race_ethnicity.nhpi"]
+
+        if uni["2015.student.demographics.race_ethnicity.two_or_more"] is not None:
+            summ += uni["2015.student.demographics.race_ethnicity.two_or_more"]
+
+        if uni["2015.student.demographics.race_ethnicity.black_non_hispanic"] is not None:
+            summ += uni["2015.student.demographics.race_ethnicity.black_non_hispanic"]
+
+        if uni["2015.student.demographics.race_ethnicity.non_resident_alien"] is not None:
+            summ += uni["2015.student.demographics.race_ethnicity.non_resident_alien"]
+
+        if uni["2015.student.demographics.race_ethnicity.white_non_hispanic"] is not None:
+            summ += uni["2015.student.demographics.race_ethnicity.white_non_hispanic"]
+
+        if uni["2015.student.demographics.race_ethnicity.unknown"] is not None:
+            summ += uni["2015.student.demographics.race_ethnicity.unknown"]
+
+        if uni["2015.student.demographics.race_ethnicity.asian_pacific_islander"] is not None:
+            summ += uni["2015.student.demographics.race_ethnicity.asian_pacific_islander"]
+
+        dic['other'] = summ
+        temp_dict[id] = dic
+        print(dic)
+
+
+    for data_curr in uni_total_data:
+        try:
+            race_dic = temp_dict[int(data_curr["university_id"])]
+
+        except Exception as e:
+            data_curr["demographics_white"] = None
+            data_curr["demographics_black"] = None
+            data_curr["demographics_asian"] = None
+            data_curr["demographics_hispanic"] = None
+            data_curr["demographics_other"] = None
+            pass
+
+        else:
+            data_curr["demographics_white"] = race_dic["white"]
+            data_curr["demographics_black"] = race_dic["black"]
+            data_curr["demographics_asian"] = race_dic["asian"]
+            data_curr["demographics_hispanic"] = race_dic["hispanic"]
+            data_curr["demographics_other"] = race_dic["other"]
+
+
+    with open('university.json', 'w') as fi:
+         json.dump(uni_total_data, fi)
+
+
+
+
+if __name__ == '__main__':
+    #get_university_stat()
+    #add_tuition_stat()
+    #scrape_race_stat()
+    add_race_stat()
