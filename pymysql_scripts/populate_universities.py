f9403801d239bf8dcb9f7a59a88e520b97da3980
unknown <mitch.traylor@utexas.edu>
2018-03-11 15:11:24 -0500

    Updated university population script to reflect changes and new values.

--- pymysql_scripts/populate_universities.py
+++ pymysql_scripts/populate_universities.py
@@ -7,7 +7,9 @@ import us_state_abbrev
 """
 What's needed here:
 -better handling for NULL values
+--solution, don't use them, just skip ahead, remove those from .json entirely
 -what if there're two colleges with the same name?
+--solution, use ID value in .json instead as PK
 
 I discovered today that there are two "Bethany College"s, one in Kansas and another in West Virginia
 """
@@ -51,6 +53,7 @@ def __main__() :
         tuitionin = 0
         tuitionout = 0
         pk = ""
+				top_majors = {}
         white = black = hispanic = asian = unknown = 0.0
         try :
             # get all data we need
@@ -65,8 +68,8 @@ def __main__() :
             tuitionin = instance["state_tuition"] + 0
             tuitionout = instance["oos_tuition"] + 0
             #image = "NULL"
-            pk = name.lower()
-            pk = pk.replace(" ", "_")
+            pk = instance["university_id"] + "" # Use university id as primary key
+						top_majors = instance["top_grad_majors"]
             white = str(instance["demographics_white"]*100)
             black = str(instance["demographics_black"]*100)
             hispanic = str(instance["demographics_hispanic"]*100)
@@ -82,7 +85,7 @@ def __main__() :
             print("NULL values given for this instance")
             continue
         
-        print("Creating a uni instance " + pk)
+        print("Creating a uni instance for " + name ", ID " + pk)
         with conn.cursor() as cursor:
             # create new university - update this statement when new info added
             sql = "INSERT INTO universities (pk, name, state, in_state_tuition, out_state_tuition) \
