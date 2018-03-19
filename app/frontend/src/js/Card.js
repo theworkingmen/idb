56b8228c2b121b3bb264782a61e9f2938117fdf1
Sungsup <smcw66@gmail.com>
2018-03-19 14:22:26 -0500

    adjusted default image size for null images in Card.js

--- app/frontend/src/js/Card.js
+++ app/frontend/src/js/Card.js
@@ -17,7 +17,11 @@ class Card extends Component {
 		    <Col xs={12} sm={4} md={4}>
 				<Thumbnail>
 					{/* img src needs to have a {local path} or {url}. DB needs to provide url of img. */}
-					{this.props.model==="colleges" ? (<Image src={`https://logo-core.clearbit.com/${this.props.domain}?size=500`} style={{width:"100%", height:"250px"}}/>) : (<Image src={this.props.domain} style={{width:"100%", height:"250px"}}/>)}
+					<center>
+					{this.props.model==="colleges" ? 
+						(<Image src={`https://logo-core.clearbit.com/${this.props.domain}?size=500`} style={{width:"18em", height:"19em"}}/>) 
+						: (<Image src={this.props.domain} style={{width:"18em", height:"19em"}}/>)}
+					</center>
 					{/*<Image src={`${this.props.imgsrc}`} style={{width:"100%", height:"100%"}}/>*/}
 					<h4><center><Link to={`/${this.props.model}/${this.props.name}`}>{this.props.name}</Link></center></h4>
 					{/* Insert component with list of attributes of the model here */}
