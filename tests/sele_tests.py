c208ab7014d7804d7cc205d9958fdee64529f635
unknown <mitch.traylor@utexas.edu>
2018-03-21 17:51:56 -0500

    Fixed selenium tests, adjusted to dynamic site.

--- tests/sele_tests.py
+++ tests/sele_tests.py
@@ -8,6 +8,7 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from random import randint
 
 
 
@@ -15,54 +16,73 @@ class SeleTests():
 
     # Create and initialize a Google Chrome webdriver to run the tests.
     def initialize(self):
-        #self.driver = webdriver.Chrome(executable_path='chrome_driver/chromedriver.exe')
-        self.driver = webdriver.Chrome('/chrome_driver.exe')
+        self.driver = webdriver.Chrome(executable_path='chrome_driver/chromedriver.exe')
+        #self.driver = webdriver.Chrome('chrome_driver.exe')
         driver = self.driver
         driver.get("http://majorpotential.me")
+        assert "Welcome to Major Potential" in driver.page_source
 
     # Test navbar - city page
     def test_navbar_city(self):
         driver = self.driver
         nav_link = driver.find_element_by_link_text("Cities")
         nav_link.click()
+        driver.implicitly_wait(3) # Wait three seconds for page to load
         assert driver.current_url == "http://majorpotential.me/cities"
-        cards = driver.find_elements_by_class_name("card-body")
+        cards = driver.find_elements_by_class_name("thumbnail")
         assert len(cards) > 0
-        assert "Universities:" in cards[0].text
-        assert "Notable job opportunities:" in cards[0].text
+        # Pick two random city cards to test.
+        random_card = randint(1, len(cards)) - 1
+        inner_html = cards[random_card].get_attribute("innerHTML")
+        assert "<a href=\"/cities/" in inner_html
+        random_card = randint(1, len(cards)) - 1
+        inner_html = cards[random_card].get_attribute("innerHTML")
+        assert "<a href=\"/cities/" in inner_html
 
     # Test navbar - university page
     def test_navbar_university(self):
         driver = self.driver
         nav_link = driver.find_element_by_link_text("Colleges")
         nav_link.click()
+        driver.implicitly_wait(3) # Wait three seconds for page to load
         assert driver.current_url == "http://majorpotential.me/colleges"
-        cards = driver.find_elements_by_class_name("card-body")
+        cards = driver.find_elements_by_class_name("thumbnail")
         assert len(cards) > 0
-        assert "Location:" in cards[0].text
-        assert "Highest Ranked Major:" in cards[0].text
+        # Pick two random university cards to test.
+        random_card = randint(1, len(cards)) - 1
+        inner_html = cards[random_card].get_attribute("innerHTML")
+        assert "<a href=\"/colleges/" in inner_html
+        random_card = randint(1, len(cards)) - 1
+        inner_html = cards[random_card].get_attribute("innerHTML")
+        assert "<a href=\"/colleges/" in inner_html
 
     # Test navbar - major page
     def test_navbar_major(self):
         driver = self.driver
         nav_link = driver.find_element_by_link_text("Majors")
         nav_link.click()
+        driver.implicitly_wait(3) # Wait three seconds for page to load
         assert driver.current_url == "http://majorpotential.me/majors"
-        cards = driver.find_elements_by_class_name("card-body")
+        cards = driver.find_elements_by_class_name("thumbnail")
         assert len(cards) > 0
-        assert "Universities:" in cards[0].text
-        assert "Most prominent city:" in cards[0].text
+        # Pick two random major cards to test.
+        random_card = randint(1, len(cards)) - 1
+        inner_html = cards[random_card].get_attribute("innerHTML")
+        assert "<a href=\"/majors/" in inner_html
+        random_card = randint(1, len(cards)) - 1
+        inner_html = cards[random_card].get_attribute("innerHTML")
+        assert "<a href=\"/majors/" in inner_html
 
     # Test navbar - about page
     def test_navbar_about(self):
         driver = self.driver
         nav_link = driver.find_element_by_link_text("About")
         nav_link.click()
-        assert "Mitchell Traylor" in driver.page_source
-        assert "Sungsup Lee" in driver.page_source
-        assert "Abel Tesfaye" in driver.page_source
-        assert "Neal Friesenhahn" in driver.page_source
-        assert "Christian Onuogu" in driver.page_source
+        driver.implicitly_wait(3) # Wait three seconds for page to load
+        inner_html = driver.find_element_by_tag_name("body").get_attribute("innerHTML")
+        assert "Bureau of Labor Statistics" in inner_html
+        assert "Amazon Beanstalk" in inner_html
+        assert "https://github.com/theworkingmen/idb" in inner_html
 
     # Close the driver
     def terminate(self):
