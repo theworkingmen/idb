58991276c3796162f2d32f68e2e253c4313fe12b
abelhtt <abelhaddistt@gmail.com>
2018-03-22 11:25:59 -0500

    remove unnecessary files

--- app/frontend/src/scrapers/Database/del.py
+++
@@ -1,7 +0,0 @@
-from base import Session, engine, Base
-from city import City
-from major import Major
-from university import University
-
-
-#Base.metadata.drop_all(engine)   # all tables are deleted
