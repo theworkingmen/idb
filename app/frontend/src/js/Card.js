801c1f0606e62f85e3f558f75d6da5f0c6584b13
Sungsup <smcw66@gmail.com>
2018-03-22 17:07:30 -0500

    cards themselves are now clickable links

--- app/frontend/src/js/Card.js
+++ app/frontend/src/js/Card.js
@@ -16,6 +16,7 @@ class Card extends Component {
 		return (
 			
 		    <Col xs={12} sm={4} md={4}>
+		    	<Link to={`/${this.props.model}/${this.props.id}`}>
 				<Thumbnail style={{height:"24em"}}>
 					{/* img src needs to have a {local path} or {url}. DB needs to provide url of img. */}
 					<center>
@@ -24,10 +25,11 @@ class Card extends Component {
 								onError={(e)=>{e.target.src="https://goo.gl/NvPJj6"}}/>
 					</center>
 					{/*<Image src={`${this.props.imgsrc}`} style={{width:"100%", height:"100%"}}/>*/}
-					<h4><center><Link to={`/${this.props.model}/${this.props.id}`}>{this.props.name}</Link></center></h4>
+					<h4><center>{this.props.name}</center></h4>
 
 					{/* Insert component with list of attributes of the model here */}
 				</Thumbnail>
+				</Link>
 			</Col>
 		);
 	}
