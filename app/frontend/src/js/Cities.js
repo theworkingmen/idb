8e9cca1fb291dc999916d032f8df8de9a4b99f85
Christian Onuogu <chrisonuogu77@gmail.com>
2018-03-27 18:26:58 -0500

    Added basic pagination for cities

--- app/frontend/src/js/Cities.js
+++ app/frontend/src/js/Cities.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Image, Grid, Row, Col, Thumbnail} from 'react-bootstrap';
+import {Image, Grid, Row, Col, Thumbnail, Pagination} from 'react-bootstrap';
 import { Link } from 'react-router-dom'
 import Card from './Card.js';
 import '../css/Flex.css';
@@ -12,8 +12,23 @@ class Cities extends Component {
     super();
     this.state = {
 		cities: [],
-		page: 0
+		pages: [],
+		page: 1
 	};
+	this.changePage = this.changePage.bind(this);
+  }
+  
+  changePage(num) {
+	  let active = num;
+	  let items = [];
+	  for (let number = 1; number <= Math.ceil(this.state.cities.length/20); number++) {
+		items.push(
+			<Pagination.Item active={number === active} onClick={this.changePage.bind(this, number)}>{number}</Pagination.Item>
+		);
+	  }
+	  this.setState({page: num,
+					 pages: items});
+			
   }
   
   componentDidMount() {
@@ -24,16 +39,32 @@ class Cities extends Component {
 			let cities = data.records.map((city) => {
 				return(<Card name={city.city_name} model='cities' domain={city.city_image_link} id={city.id}>  </Card>)
 			})
+			let active = 1;
+			let items = [];
+			for (let number = 1; number <= Math.ceil(cities.length/20); number++) {
+				items.push(
+					<Pagination.Item active={number === active} onClick={this.changePage.bind(this, number)}>{number}</Pagination.Item>
+				);
+			}
+			this.setState({pages: items});
 			this.setState({cities: cities});
 		})
+		
+
   }
 
   render() {
 	let display = []
 	for (let i = 0; i < 20; i++) {
-		display[i] = this.state.cities[(this.state.page * 20) + i]
+		display[i] = this.state.cities[((this.state.page - 1) * 20) + i]
 	}
-    return <Grid><Row className="flex-row">{display}</Row></Grid>;
+	
+    return (
+		<div>
+		<Grid><Row className="flex-row">{display}</Row></Grid>
+		<center><Pagination bsSize="large">{this.state.pages}</Pagination></center>
+		</div>
+		)
   }
 }
 
