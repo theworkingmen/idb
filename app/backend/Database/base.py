18f0630c7c1c50b0808975566e171a6aba6c8339
abelhtt <abelhaddistt@gmail.com>
2018-03-29 20:26:38 -0500

    New database with majors' average_wage and total_people_in_workforce are integers

--- app/backend/Database/base.py
+++ app/backend/Database/base.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-engine = create_engine('postgresql://theworkingmen:cs373downing@majorpotential-database.coujqf2v990h.us-east-1.rds.amazonaws.com/majorpotential')
+engine = create_engine('postgresql://theworkingmen:cs373downing@majorpotential-new.coujqf2v990h.us-east-1.rds.amazonaws.com/majorpotential')
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
