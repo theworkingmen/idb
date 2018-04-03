e3b1770d1eedf9024d8463e13b4f1ea14033d8c4
Sungsup <smcw66@gmail.com>
2018-04-03 18:16:00 -0500

    Created TallCard component

---
+++ app/frontend/src/js/TallCard.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import {Image, Col, Thumbnail} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+
+class TallCard extends Component {
+
+	constructor(props) {
+		super(props);
+
+	}
+
+	/* default img src = https://goo.gl/NvPJj6 */
+
+	render() {
+		return (
+
+		    <Col xs={12} sm={3} md={3} lg={3}>
+		    	<Link to={`/${this.props.model}/${this.props.id}`}>
+				<Thumbnail style={{height:"23em"}}>
+					{/* img src needs to have a {local path} or {url}. DB needs to provide url of img. */}
+					<center>
+						<Image 	src={this.props.domain}
+								style={{width:"14em", height:"14em"}}
+								onError={(e)=>{e.target.src="https://goo.gl/NvPJj6"}}/>
+					</center>
+					{/*<Image src={`${this.props.imgsrc}`} style={{width:"100%", height:"100%"}}/>*/}
+					<h4><center style={{justifyContent:"center", height:"4em"}}>{this.props.name}</center></h4>
+					<h6><center style={{justifyContent:"center"}}>{this.props.field}</center></h6>
+
+					{/* Insert component with list of attributes of the model here */}
+				</Thumbnail>
+				</Link>
+			</Col>
+		);
+	}
+
+}
+
+export default TallCard;
