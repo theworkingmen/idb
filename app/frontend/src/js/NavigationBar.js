4a1311d72cf3f796c17145ac7f6d5ed5f1f66653
Sungsup <smcw66@gmail.com>
2018-04-03 19:48:10 -0500

    pressing enter in search bar now triggers the search

--- app/frontend/src/js/NavigationBar.js
+++ app/frontend/src/js/NavigationBar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router'
 import { Navbar, Nav, NavItem, FormGroup, FormControl, Button} from 'react-bootstrap';
 import { Link } from 'react-router-dom'
 
@@ -7,6 +8,7 @@ class NavigationBar extends Component {
 		super(props);
 		this.state = {highlight: props.highlight,
 					  search: "search",
+					  redirect: false,
 					  value: ''};
 	}
 	
@@ -14,8 +16,18 @@ class NavigationBar extends Component {
 		this.setState({ value: e.target.value });
 	}
 
+	handleKeyPress(e) {
+	    if (e.key === 'Enter') {
+			this.setState({redirect:true})
+			}	
+		}
 
 	render() {
+		var redirection = null;
+		if (this.state.redirect) {
+			redirection = <Redirect to={`/${this.state.search}/${this.state.value}`} />;
+			this.setState({redirect:false})
+		}
 		return (
 		    <div className="NavigationBar">
 		        <Navbar inverse collapseOnSelect>
@@ -42,9 +54,11 @@ class NavigationBar extends Component {
 					</Nav>
 					<Navbar.Form pullRight>
 						<FormGroup>
-							<FormControl type="text" placeholder="Search" value={this.state.value} onChange={this.handleChange.bind(this)} />
+							<FormControl type="text" placeholder="Search" value={this.state.value} 
+										 onChange={this.handleChange.bind(this)} onKeyPress={this.handleKeyPress.bind(this)} />
 						</FormGroup>{' '}
 						<Link to={`/${this.state.search}/${this.state.value}`}><Button type="submit">Search</Button></Link>
+						{redirection}
 					</Navbar.Form>
 					</Navbar.Collapse>
 				</Navbar>
--- app/frontend/src/js/TallCard.js
+++ app/frontend/src/js/TallCard.js
@@ -17,7 +17,7 @@ class TallCard extends Component {
 
 		    <Col xs={12} sm={3} md={3} lg={3}>
 		    	<Link to={`/${this.props.model}/${this.props.id}`}>
-				<Thumbnail style={{height:"23em"}}>
+				<Thumbnail style={{height:"30em"}}>
 					{/* img src needs to have a {local path} or {url}. DB needs to provide url of img. */}
 					<center>
 						<Image 	src={this.props.domain}
@@ -26,6 +26,10 @@ class TallCard extends Component {
 					</center>
 					{/*<Image src={`${this.props.imgsrc}`} style={{width:"100%", height:"100%"}}/>*/}
 					<h4><center style={{justifyContent:"center", height:"4em"}}>{this.props.name}</center></h4>
+					<h6><center style={{justifyContent:"center"}}>{this.props.field}</center></h6>
+					<h6><center style={{justifyContent:"center"}}>{this.props.field}</center></h6>
+					<h6><center style={{justifyContent:"center"}}>{this.props.field}</center></h6>
+					<h6><center style={{justifyContent:"center"}}>{this.props.field}</center></h6>
 					<h6><center style={{justifyContent:"center"}}>{this.props.field}</center></h6>
 
 					{/* Insert component with list of attributes of the model here */}
